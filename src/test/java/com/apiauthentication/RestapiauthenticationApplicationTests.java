bc011f4334de82f022d2bf8a92c627c322eac874
Ashish Sam <ashishsamtgeorge@mca.ajce.in>
2022-08-10 16:20:53 +0530

    Create Registration and Login flow

---
+++ src/test/java/com/apiauthentication/RestapiauthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiauthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
