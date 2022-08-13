dff0626569c4799c512cf5247ca285392a60eaf2
Ashish Sam <ashishsamtgeorge@mca.ajce.in>
2022-08-13 11:52:02 +0530

    Initialize Spring Application

---
+++ spring-cloudstore/src/main/java/com/cloudstore/SpringCloudstoreApplication.java
@@ -0,0 +1,13 @@
+package com.cloudstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudstoreApplication.class, args);
+	}
+
+}
---
+++ spring-cloudstore/src/test/java/com/cloudstore/SpringCloudstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
